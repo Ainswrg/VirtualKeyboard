3579af10fb3be74b719614ac134cdd4862cd8390
Jaslan <Jaslan.dlnv@gmail.com>
2022-05-09 23:34:59 +0500

    feat: implement print keys

--- src/core/app/App.js
+++ src/core/app/App.js
@@ -1,4 +1,4 @@
-import Keyboard from '../pages/keyboard';
+import KeyboardPage from '../pages/keyboard';
 import Data from '../data/language';
 
 const dataLang = new Data();
@@ -8,7 +8,12 @@ class App {
     this.data = '';
     this.language = '';
     this.container = document.body;
-    this.keyboardContainer = document.querySelector('.keyboard');
+    this.display = '';
+  }
+
+  keyboardPage(data = this.data, language = this.language) {
+    const page = new KeyboardPage(data, language);
+    return page;
   }
 
   setLocalStorage(value = this.language) {
@@ -27,16 +32,17 @@ class App {
   }
 
   renderNewPage() {
-    const page = new Keyboard(this.data, this.language);
+    const page = this.keyboardPage(this.data, this.language);
     const pageHtml = page.render();
     this.container.append(pageHtml);
   }
 
   updateKeyboard(language, data = this.data) {
-    this.container.innerHTML = '';
-    const page = new Keyboard(data, language);
-    const pageHtml = page.render();
-    this.container.append(pageHtml);
+    const keyboard = document.querySelector('.keyboard');
+    keyboard.innerHTML = '';
+    const page = this.keyboardPage(data, language);
+    const pageHtml = page.generateKeyboard();
+    keyboard.append(pageHtml);
   }
 
   capsLock() {
@@ -83,18 +89,157 @@ class App {
     return this.language;
   }
 
-  addListeners() {
-    window.addEventListener('keydown', (e) => {
-      e.preventDefault();
-      if (e.shiftKey) {
-        if (e.key === 'Alt') {
+  printKeys(keyCode, value, option) {
+    this.display = document.querySelector('#textarea');
+    let cursor = this.display.selectionStart;
+    const left = this.display.value.slice(0, cursor);
+    const right = this.display.value.slice(cursor);
+
+    let select;
+    const start = this.display.selectionStart;
+    const end = this.display.selectionEnd;
+
+    select = start === end ? null : this.display.value.slice(start, end);
+    const displayVal = (sel, val, opt) => {
+      if (sel) {
+        this.display.setRangeText((select = val));
+      } else if (opt === 'backspace') {
+        this.display.value = left.slice(0, -1) + right;
+      } else if (opt === 'delete') {
+        this.display.value = left + right.slice(1);
+      } else {
+        this.display.value = `${left}${val}${right}`;
+      }
+    };
+
+    switch (keyCode) {
+      case 'CapsLock': {
+        this.capsLock();
+        break;
+      }
+      case 'Backspace': {
+        const backspace = '';
+        displayVal(select, backspace, 'backspace');
+        cursor -= 1;
+        break;
+      }
+      case 'Delete': {
+        const del = '';
+        displayVal(select, del, 'delete');
+        break;
+      }
+      case 'Enter': {
+        const enter = '\n';
+        displayVal(false, enter);
+        cursor += 1;
+        break;
+      }
+      case 'Tab': {
+        const tab = '\t';
+        displayVal(select, tab);
+        cursor += 1;
+        break;
+      }
+      case 'Space': {
+        const space = ' ';
+        displayVal(select, space);
+        cursor += 1;
+        break;
+      }
+      case 'ArrowLeft': {
+        cursor = cursor - 1 >= 0 ? cursor - 1 : 0;
+        break;
+      }
+      case 'ArrowRight': {
+        cursor += 1;
+        break;
+      }
+      case 'ArrowDown': {
+        const arrRows = right.split('\n');
+        const row = arrRows.length - (arrRows.length - 1);
+        if (arrRows[row]) {
+          cursor += arrRows[row].length + 1;
+        } else {
+          return;
+        }
+        break;
+      }
+      case 'ArrowUp': {
+        const arrRows = left.split('\n');
+        let row = arrRows.length - 1;
+        if (row >= 1) {
+          cursor -= arrRows[row].length + 1;
+          row -= 1;
+        }
+        break;
+      }
+      case 'ShiftLeft':
+      case 'ShiftRight': {
+        if (option === 'shift') {
+          const keyArr = document.querySelectorAll('.letter');
+          keyArr.forEach((keyLetter) => {
+            keyLetter.classList.add('upperCase');
+          });
+        }
+        break;
+      }
+      case 'AltLeft':
+      case 'AltRight': {
+        if (option === 'shift') {
           const currLang = this.switchLanguage();
           this.setLocalStorage(currLang);
         }
+        break;
       }
-      if (e.key === 'CapsLock') {
-        this.capsLock();
+      case 'ControlLeft':
+      case 'ControlRight': {
+        break;
       }
+      case 'MetaLeft':
+      case 'MetaRight': {
+        break;
+      }
+      default: {
+        displayVal(false, value);
+        cursor += 1;
+      }
+    }
+    this.display.focus();
+    this.display.setSelectionRange(cursor, cursor);
+  }
+
+  addListeners() {
+    window.addEventListener('keydown', (e) => {
+      e.preventDefault();
+
+      this.data.forEach((key) => {
+        if (e.code === key.code) {
+          const keyId = document.querySelector(`#${key.code}`);
+          keyId.classList.add('active');
+          if (e.shiftKey) {
+            const currKey = key.key.toUpperCase() || key.key__first;
+            this.printKeys(key.code, currKey, 'shift');
+          } else if (e.ctrlKey) {
+            const currKey = key.key || key.key__second;
+            this.printKeys(key.code, currKey, 'ctrl');
+          } else {
+            const currKey = key.key || key.key__second;
+            this.printKeys(key.code, currKey);
+          }
+        }
+      });
+    });
+    window.addEventListener('keyup', (e) => {
+      const keyArr = document.querySelectorAll('.letter');
+      keyArr.forEach((keyLetter) => {
+        keyLetter.classList.remove('upperCase');
+      });
+      this.data.forEach((key) => {
+        if (e.code === key.code) {
+          const keyId = document.querySelector(`#${key.code}`);
+          keyId.classList.remove('active');
+        }
+      });
     });
   }
 
