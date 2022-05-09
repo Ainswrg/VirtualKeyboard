0a03c1a2065f21559f9e6c800aaf2123b1401c84
Jaslan <Jaslan.dlnv@gmail.com>
2022-05-09 23:26:39 +0500

    refactor: clear and hide methods

--- src/core/app/App.js
+++ src/core/app/App.js
@@ -1,10 +1,29 @@
 import Keyboard from '../pages/keyboard';
+import Data from '../data/language';
 
+const dataLang = new Data();
+const [engData, ruData] = dataLang.getData();
 class App {
-  constructor(data, language) {
+  constructor() {
+    this.data = '';
+    this.language = '';
+    this.container = document.body;
+    this.keyboardContainer = document.querySelector('.keyboard');
+  }
+
+  setLocalStorage(value = this.language) {
+    localStorage.setItem('language', JSON.stringify(value));
+  }
+
+  getLocalStorage() {
+    const lang = JSON.parse(localStorage.getItem('language'));
+    this.language = lang;
+    return lang;
+  }
+
+  updateDataAndLang(data, language) {
     this.data = data;
     this.language = language;
-    this.container = document.body;
   }
 
   renderNewPage() {
@@ -21,30 +40,30 @@ class App {
   }
 
   capsLock() {
-    const updateData = this.data.map((e) => {
-      if (e.className === 'key letter') {
-        if (e.key) {
-          if (e.key === e.key.toUpperCase()) {
-            e.key = e.key.toLowerCase();
-          } else {
-            e.key = e.key.toUpperCase();
+    const dataToCaps = (data) => {
+      const res = data.map((e) => {
+        if (e.className === 'key letter') {
+          if (e.key) {
+            if (e.key === e.key.toUpperCase()) {
+              e.key = e.key.toLowerCase();
+            } else {
+              e.key = e.key.toUpperCase();
+            }
           }
         }
-        if (e.keyRU) {
-          if (e.keyRU === e.keyRU.toUpperCase()) {
-            e.keyRU = e.keyRU.toLowerCase();
-          } else {
-            e.keyRU = e.keyRU.toUpperCase();
-          }
+        if (e.className === 'key caps-lock') {
+          e.className = 'key caps-lock focused';
+        } else if (e.className === 'key caps-lock focused') {
+          e.className = 'key caps-lock';
         }
-      }
-      if (e.className === 'key caps-lock') {
-        e.className = 'key caps-lock focused';
-      } else if (e.className === 'key caps-lock focused') {
-        e.className = 'key caps-lock';
-      }
-      return e;
-    });
+        return e;
+      });
+      return res;
+    };
+    const ru = dataToCaps(ruData);
+    const eng = dataToCaps(engData);
+
+    const updateData = this.language === 'ENG' ? eng : ru;
 
     this.updateKeyboard(this.language, updateData);
   }
@@ -52,16 +71,38 @@ class App {
   switchLanguage() {
     if (this.language === 'ENG') {
       this.language = 'RU';
-      this.updateKeyboard(this.language);
+      const newData = this.language === 'ENG' ? engData : ruData;
+      this.updateDataAndLang(newData, this.language);
+      this.updateKeyboard(this.language, this.data);
     } else if (this.language === 'RU') {
       this.language = 'ENG';
-      this.updateKeyboard(this.language);
+      const newData = this.language === 'ENG' ? engData : ruData;
+      this.updateDataAndLang(newData, this.language);
+      this.updateKeyboard(this.language, this.data);
     }
-
     return this.language;
   }
 
+  addListeners() {
+    window.addEventListener('keydown', (e) => {
+      e.preventDefault();
+      if (e.shiftKey) {
+        if (e.key === 'Alt') {
+          const currLang = this.switchLanguage();
+          this.setLocalStorage(currLang);
+        }
+      }
+      if (e.key === 'CapsLock') {
+        this.capsLock();
+      }
+    });
+  }
+
   run() {
+    const languageLocal = this.getLocalStorage() || 'ENG';
+    const dataLanguage = languageLocal === 'ENG' ? engData : ruData;
+    this.updateDataAndLang(dataLanguage, languageLocal);
+    this.addListeners();
     this.renderNewPage();
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
 import 'normalize.css';
 import './index.scss';
-import getData from './core/data/language';
 import App from './core/app/App';
-import LocalLanguage from './utils/local';
-
-const data = getData();
-const lang = new LocalLanguage();
-const language = lang.getLocalStorage() || 'ENG';
 
 window.onload = () => {
-  if (data) {
-    const app = new App(data, language);
-    app.run();
-
-    window.addEventListener('keydown', (e) => {
-      e.preventDefault();
-      if (e.shiftKey) {
-        if (e.key === 'Alt') {
-          const currLang = app.switchLanguage();
-          lang.setLocalStorage(currLang);
-        }
-      }
-      if (e.key === 'CapsLock') {
-        app.capsLock();
-      }
-    });
-  }
+  const app = new App();
+  app.run();
 };
